3b7a28a407de269e3f38f193a729be9d1e59a5c1
Jonathan Himbing <nathanjo86@gmail.com>
2017-09-29 10:19:08 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # pa11y_scan_auth
+
 [WIP] Scann Pa11y sites with session-based authentication
+
+# Instructions
+
+- Install the following gem:
+
+* gem install nokogiri
+* gem install mechanize - http://docs.seattlerb.org/mechanize/
+
+- Create a site directory within the root (This is where the scraped pages and pa11y results will be stored)
+
+- To execute:
+
+```
+ruby auth_script.rb
+```
+
+# Configuration
+
+Some modification of the script is needed.
+Most of the sites has different login form field elements, if the field provided didn't work, you need to do some inspect element and get the correct field name.
+
+Use your favorite editor to edit the file auth_script.rb.
+
+### URL of the login page
+
+Example:
+
+login = agent.get('https://drupal.org/user/login').form
+
+### Provide login username
+
+Example:
+
+login.field.value = "nathanjo"
+
+### Provide login password
+
+Example:
+
+login.pass = "YOUR_PASSWORD"
---
+++ auth_script.rb
@@ -0,0 +1,32 @@
+#!/Users/nathanjo/.rbenv/shims/ruby
+
+require 'nokogiri'
+require 'mechanize'
+require 'fileutils'
+
+agent = Mechanize.new
+
+puts "getting the login form"
+login = agent.get('https://drupal.org/user/login').form
+puts "Preparing form"
+puts "username: nathanjo"
+login.field.value = "YOUR_USERNAME"
+puts "password: ****"
+login.pass = "YOUR_PASSWORD"
+puts "submitting form"
+page = agent.submit(login, login.buttons.first)
+
+page.links.each do |link|
+  # assume relative path for drupal
+  if link.href[0] == "/" && !(link.href.include? "logout") && link.href.size > 1
+    puts "clicking the link >>> #{link.href}"
+    px = agent.page.link_with(:href => link.href).click
+    html_doc = Nokogiri::HTML(px.body)
+    filename = link.text.gsub!(/[^0-9A-Za-z]/, '')
+    fileHtml = File.new("site/#{filename}.html", "w+")
+    fileHtml.puts html_doc
+    fileHtml.close()
+    puts "pa11y 'file:///#{Dir.pwd}/#{filename}.html'"
+    system "pa11y 'file:///#{Dir.pwd}/site/#{filename}.html' -r html > site/#{filename}_result.html"
+  end
+end
